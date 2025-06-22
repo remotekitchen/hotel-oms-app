b9020769eee7b67f5da2b1d87f87107b2c57981a
nazmul162001 <nazmulprime162001@gmail.com>
2025-06-22 23:27:32 +0600

    fix and updated nativewind setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -20,7 +20,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <View className="flex-1 items-center justify-center bg-white">
-          <Text className="text-4xl font-bold text-red-500">
+          <Text className="text-2xl font-bold text-red-500">
             Welcome to Nativewind!
           </Text>
         </View>
